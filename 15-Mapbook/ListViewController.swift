c13d9f0d29a6bea4fe000c3ded49cee73100dbeb
selcuk baki <selcukbaki@windowslive.com>
2021-08-28 22:38:39 +0300

    github test commit

--- 15-Mapbook/ListViewController.swift
+++ 15-Mapbook/ListViewController.swift
@@ -6,24 +6,85 @@
 //
 
 import UIKit
+import CoreData
 
-class ListViewController: UIViewController {
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var idArray = [UUID]()
+    var titleArray = [String]()
+    var chosenTitle = ""
+    var chosenTitleID : UUID?
+
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClick))
+        
+        getData()
+        tableView.reloadData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name("newPlace"), object: nil)
+    }
+    
+   @objc func getData(){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Places")
+        request.returnsObjectsAsFaults = false
+        do {
+            let resultsArray = try context.fetch(request)
+            if resultsArray.count > 0 {
+                self.titleArray.removeAll(keepingCapacity: false)
+                self.idArray.removeAll(keepingCapacity: false)
+            for result in resultsArray as! [NSManagedObject] {
+                if let title = result.value(forKey: "title") as? String{
+                    self.titleArray.append(title)
+                }
+                if let id = result.value(forKey: "id") as? UUID {
+                    self.idArray.append(id)
+                }
+                self.tableView.reloadData()
+            }
+        }
+        } catch  {
+            print("error")
+        }
+    }
+    
+    @objc func addButtonClick(){
+        performSegue(withIdentifier: "toViewController", sender: nil)
+        chosenTitle = ""
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titleArray.count
     }
-    */
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = titleArray[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        chosenTitle = titleArray[indexPath.row]
+        chosenTitleID = idArray[indexPath.row]
+        performSegue(withIdentifier: "toViewController", sender: nil)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toViewController" {
+            let destinationVC = segue.destination as! ViewController
+            destinationVC.selectedTitle = chosenTitle
+            destinationVC.selectedTitleID = chosenTitleID
+            
+        }
+    }
 }
--- 15-Mapbook/ViewController.swift
+++ 15-Mapbook/ViewController.swift
@@ -6,14 +6,158 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+import CoreData
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var commentText: UITextField!
+    
+    
+    var locationManager = CLLocationManager()
+    var chosenLatitude = Double()
+    var chosenLongitude = Double()
+    
+    var selectedTitle = ""
+    var selectedTitleID : UUID?
+    
+    var annotationTitle = ""
+    var annotationSubtitle = ""
+    var annotationLatitude = Double()
+    var annotationLongitude = Double()
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        addButton.isHidden = true
+        mapView.delegate = self
+        locationManager.delegate = self
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(chooseLocation(gestureRecognizer:)))
+        gestureRecognizer.minimumPressDuration = 2
+        mapView.addGestureRecognizer(gestureRecognizer)
+        
+        if selectedTitle != "" {
+            nameText.isUserInteractionEnabled = false
+            commentText.isUserInteractionEnabled = false
+            addButton.isHidden = true
+            //COREDATA
+            
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Places")
+            let idString = selectedTitleID!.uuidString
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+            let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject]{
+                       
+                        if let title = result.value(forKey: "title") as? String {
+                            annotationTitle = title
+                            
+                            if let subtitle = result.value(forKey: "subtitle") as? String {
+                                annotationSubtitle = subtitle
+                                
+                                if let latitude = result.value(forKey: "latitude") as? Double {
+                                    annotationLatitude = latitude
+                                    
+                                    if let longitude = result.value(forKey: "longitude") as? Double {
+                                        annotationLongitude = longitude
+                                        
+                                        let annotation = MKPointAnnotation()
+                                        annotation.title = annotationTitle
+                                        annotation.subtitle = annotationSubtitle
+                                        
+                                        let coordinate = CLLocationCoordinate2DMake(annotationLatitude, annotationLongitude)
+                                        annotation.coordinate = coordinate
+                                        mapView.addAnnotation(annotation)
+                                        nameText.text = annotationTitle
+                                        commentText.text = annotationSubtitle
+                                        
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch{
+                print("error")
+            }
+        }else{
+            //ADD NEW DATA
+            addButton.isHidden = false
+
+        }
     }
+    
+    @objc func chooseLocation(gestureRecognizer : UILongPressGestureRecognizer){
+        
+        if gestureRecognizer.state == .began {
+            
+            let touchedPoint = gestureRecognizer.location(in: self.mapView)
+            let touchedCoordinate = self.mapView.convert(touchedPoint, toCoordinateFrom: self.mapView)
+            
+            chosenLatitude = touchedCoordinate.latitude
+            chosenLongitude = touchedCoordinate.longitude
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = touchedCoordinate
+            annotation.title = nameText.text
+            annotation.subtitle = commentText.text
+            self.mapView.addAnnotation(annotation)
+            
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if selectedTitle == "" {
+            let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+            let span = MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03)
+            let region = MKCoordinateRegion(center: location, span: span)
+            mapView.setRegion(region, animated: true)
+        }else{
+    }
+}
 
-
+    @IBAction func saveClicked(_ sender: Any) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newPlace = NSEntityDescription.insertNewObject(forEntityName: "Places", into: context)
+      
+        newPlace.setValue(nameText.text, forKey: "title")
+        newPlace.setValue(commentText.text, forKey: "subtitle")
+        newPlace.setValue(chosenLongitude, forKey: "longitude")
+        newPlace.setValue(chosenLatitude, forKey: "latitude")
+        newPlace.setValue(UUID(), forKey: "id")
+        
+        do {
+            try context.save()
+            print("success")
+        } catch  {
+            print("error")
+        }
+        
+        NotificationCenter.default.post(name: NSNotification.Name("newPlace"), object: nil)
+        navigationController?.popViewController(animated: true)
+        
+    }
 }
 
